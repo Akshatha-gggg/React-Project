0ef2aa68adb7932f931f1540a8f04eb6b0da4d0f
Akshatha-gggg <akshatharanganath@virtusa.com>
2022-03-24 18:00:40 +0530

    completed

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,33 @@
+import React, {useContext} from 'react';
+import { Grid, Divider, Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+
+import { ExpenseTrackerContext } from '../../context/context';
+import Form from './Form/Form';
+import List from './List/List';
+import useStyle from './style';
+
+const Main = () => {
+    const classes = useStyle();
+    const {balance} = useContext(ExpenseTrackerContext)
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker"/>
+            <CardContent>
+                <Typography align="center" variant="h5">Total Balance Rs { balance }</Typography>
+                
+                <Divider />
+                <Form />
+            </CardContent>
+            <CardContent className={classes.cardContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <List />
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main;
